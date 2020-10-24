873cb41f43c31439cef69600963f994224f50cd3
KubenQPL <19orton20@gmail.com>
2020-10-24 17:22:12 +0200

    fix listening to text change

--- src/main/kotlin/tech/stonks/kvizard/utils/swing.kt
+++ src/main/kotlin/tech/stonks/kvizard/utils/swing.kt
@@ -5,13 +5,17 @@ import javax.swing.event.DocumentEvent
 import javax.swing.event.DocumentListener
 
 fun JTextField.setOnTextChangedListener(onTextChanged: (text: String) -> Unit) {
-    this.document.addDocumentListener(object: DocumentListener {
-        override fun insertUpdate(e: DocumentEvent?) = Unit
+    this.document.addDocumentListener(object : DocumentListener {
+        override fun insertUpdate(e: DocumentEvent?) {
+            onTextChanged(this@setOnTextChangedListener.text)
+        }
 
-        override fun removeUpdate(e: DocumentEvent?) = Unit
+        override fun removeUpdate(e: DocumentEvent?) {
+            onTextChanged(this@setOnTextChangedListener.text)
+        }
 
         override fun changedUpdate(e: DocumentEvent?) {
-            onTextChanged(e?.document?.getText(0, document.length) ?: "")
+            onTextChanged(this@setOnTextChangedListener.text)
         }
     })
 }
